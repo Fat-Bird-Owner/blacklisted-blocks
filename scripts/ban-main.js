5d0c5855af931941f649abac1b72a89d81a14f85
Fat Bird’s Owner <lykongvibol2012@gmail.com>
2026-08-07 11:45:11 +0700

    Update ban-main.js

--- scripts/ban-main.js
+++ scripts/ban-main.js
@@ -1,16 +1,19 @@
 Events.on(PlayEvent, () => {
 try {
 
+if (!Core.settings.getBool("bb-enabled")) return;
+  
 let text = "Banned:"
 let bans = new ObjectSet();
 let techTree = Vars.state.rules.planet.techTree
+let amount = Core.settings.getInt("bb-amount");
 
 if (!techTree || Vars.state.rules.planet != Planets.serpulo) return;
 
 techTree.each(n => {
 try {
 
-if ((Mathf.random() * 100) >= 75 && bans.size() <= 25){
+if ((Mathf.random() * 100) >= 75 && bans.size() <= amount){
 bans.add(n.content)
 text = text + " " + n.content.emoji()
 }
